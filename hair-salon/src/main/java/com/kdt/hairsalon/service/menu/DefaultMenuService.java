49a949b5442ba2294367c00cd3c7affa48da4798
pjh612 <pjhjh612@gmail.com>
2022-05-02 17:23:57 +0900

    test : DefaultMenuService Test 추가

--- hair-salon/src/main/java/com/kdt/hairsalon/service/menu/DefaultMenuService.java
+++ hair-salon/src/main/java/com/kdt/hairsalon/service/menu/DefaultMenuService.java
@@ -11,6 +11,7 @@ import java.util.stream.Collectors;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,20 +20,21 @@ public class DefaultMenuService implements MenuService {
     private final MenuRepository menuRepository;
 
 
+    @Transactional
     @Override
     public MenuDto insert(String name, int price) {
         Menu menu = menuRepository.insert(new Menu(UUID.randomUUID(), name, price, LocalDateTime.now(), LocalDateTime.now()));
 
         return MenuDto.of(menu);
     }
-
+    @Transactional(readOnly = true)
     @Override
     public MenuDto findById(UUID id) {
         Menu menu = menuRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 ID로 메뉴 정보를 찾을 수 없습니다."));
 
         return MenuDto.of(menu);
     }
-
+    @Transactional(readOnly = true)
     @Override
     public List<MenuDto> findAll() {
         return menuRepository.findAll()
---
+++ hair-salon/src/test/java/com/kdt/hairsalon/service/menu/DefaultMenuServiceTest.java
@@ -0,0 +1,91 @@
+package com.kdt.hairsalon.service.menu;
+
+import static com.wix.mysql.EmbeddedMysql.anEmbeddedMysql;
+import static com.wix.mysql.ScriptResolver.classPathScript;
+import static com.wix.mysql.config.MysqldConfig.aMysqldConfig;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.wix.mysql.EmbeddedMysql;
+import com.wix.mysql.config.Charset;
+import com.wix.mysql.config.MysqldConfig;
+import com.wix.mysql.distribution.Version;
+
+import java.util.List;
+
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Transactional
+class DefaultMenuServiceTest {
+
+    @Autowired
+    private MenuService menuService;
+
+    private  EmbeddedMysql embeddedMySql;
+
+    @BeforeAll
+    void setup() {
+        MysqldConfig config = aMysqldConfig(Version.v5_7_latest)
+                .withCharset(Charset.UTF8)
+                .withPort(2215)
+                .withUser("test", "test1234!")
+                .withTimeZone("Asia/Seoul")
+                .build();
+        embeddedMySql = anEmbeddedMysql(config)
+                .addSchema("test-hair_salon", classPathScript("schema.sql"))
+                .start();
+
+    }
+
+    @AfterAll
+     void cleanUp() {
+        embeddedMySql.stop();
+    }
+
+    @Test
+    @DisplayName("정상 시술 메뉴 추가 테스트")
+    void insertTest() {
+        //when
+        MenuDto menuA = menuService.insert("menuA", 12000);
+        MenuDto foundMenu = menuService.findById(menuA.getId());
+
+        //then
+        assertThat(foundMenu, samePropertyValuesAs(menuA));
+    }
+
+    @Test
+    @DisplayName("시술 메뉴 ID로 조회 테스트")
+    void findByIdTest() {
+        //given
+        MenuDto menuA = menuService.insert("menuA", 12000);
+
+
+        //when
+        MenuDto foundMenu = menuService.findById(menuA.getId());
+        //then
+        assertThat(foundMenu, samePropertyValuesAs(menuA));
+    }
+
+    @Test
+    @DisplayName("시술 메뉴 전체 조회 테스트")
+    void findAllTest() {
+        //given
+        MenuDto menuA = menuService.insert("menuA", 12000);
+        MenuDto menuB = menuService.insert("menuB", 12000);
+
+        //when
+        List<MenuDto> foundMenus = menuService.findAll();
+
+        //then
+        assertThat(foundMenus.size(), is(2));
+        assertThat(foundMenus, containsInAnyOrder(samePropertyValuesAs(menuA),samePropertyValuesAs(menuB)));
+    }
+}
