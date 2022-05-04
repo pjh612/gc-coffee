af5ab1f5cc38488aa2b6411306d7155b79e2a4e6
jinhyung <pjh_jn@naver.com>
2022-05-04 15:53:45 +0900

    feat : 리액트 메인 컴포넌트 App.js 추가

---
+++ kdt-react-order-ui/src/App.js
@@ -0,0 +1,100 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import React, {useEffect, useState} from 'react';
+
+import axios from "axios";
+import {DesignerList} from "./components/DesignerList";
+import {MenuList} from "./components/MenuList";
+import {DesignerAdd} from "./components/DesignerAdd";
+import {MenuAdd} from "./components/MenuAdd";
+import {AppointmentList} from "./components/AppointmentList";
+import {AppointmentAdd} from "./components/AppointmentAdd";
+import {CustomerList} from "./components/CustomerList";
+import {CustomerAdd} from "./components/CustomerAdd";
+
+function App() {
+    const [menus, setMenus] = useState([
+        {id: 'uuid-1', name: '콜롬비아 커피 1', price: 5000},
+        {id: 'uuid-2', name: '콜롬비아 커피 2', price: 5000},
+        {id: 'uuid-3', name: '콜롬비아 커피 3', price: 5000}
+    ]);
+    const [designers, setDesigners] = useState([
+        {id: 'uuid-1', name: 'park', position: 'INTERN', joinedAt: ''},
+        {id: 'uuid-2', name: 'lee', position: 'INTERN', joinedAt: ''},
+        {id: 'uuid-3', name: 'kim', position: 'INTERN', joinedAt: ''},
+    ]);
+
+    const [appointments, setAppointments] = useState([
+        {appointmentId: 'uuid-1', customerId: '', menuId: '', designerId: '', appointedAt:''},
+    ]);
+
+    const [customers, setCustomers] = useState([
+        {id: 'uuid-1', name: '', email: '', gender: '', birth:''},
+    ]);
+
+    const onMenuAdd = o => {
+        setMenus(menus.concat(o));
+    };
+    const onCustomerAdd = o => {
+        setCustomers(customers.concat(o));
+    };
+    const onDesignerAdd = o => {
+        setDesigners(designers.concat(o));
+    };
+    const onAppointmentAdd = o => {
+        setAppointments(appointments.concat(o));
+    };
+    useEffect(() => {
+        axios.get('http://localhost:8080/api/v1/designers')
+            .then(v => setDesigners(v.data));
+        axios.get('http://localhost:8080/api/v1/menus')
+            .then(v => setMenus(v.data));
+        axios.get('http://localhost:8080/api/v1/appointments')
+            .then(v => setAppointments(v.data));
+        axios.get('http://localhost:8080/api/v1/customers')
+            .then(v => setCustomers(v.data));
+    }, []);
+
+
+
+    return (
+        <div className="container-fluid">
+            <div className="row justify-content-center m-4">
+                <h1 className="text-center">Hello Hair Salon</h1>
+            </div>
+            <div className="card">
+                <div className="row">
+                    <div className="col-md-8 mt-4 d-flex flex-column align-items-start p-3 pt-0">
+                        <DesignerList designers={designers} setDesigners={setDesigners}/>
+                    </div>
+                    <div className="col-md-4 summary p-4">
+                        <DesignerAdd onAdd={onDesignerAdd} />
+                    </div>
+                    <div className="col-md-8 mt-4 d-flex flex-column align-items-start p-3 pt-0">
+                        <MenuList menus={menus} setMenus={setMenus}/>
+                    </div>
+                    <div className="col-md-4 summary p-4">
+                        <MenuAdd onAdd={onMenuAdd} />
+                    </div>
+                    <div className="col-md-8 mt-4 d-flex flex-column align-items-start p-3 pt-0">
+                        <AppointmentList appointments={appointments} setAppointments={setAppointments}/>
+                    </div>
+                    <div className="col-md-4 summary p-4">
+                        <AppointmentAdd onAdd={onAppointmentAdd} />
+                    </div>
+
+                    <div className="col-md-8 mt-4 d-flex flex-column align-items-start p-3 pt-0">
+                        <CustomerList customers={customers} setCustomers={setCustomers}/>
+                    </div>
+                    <div className="col-md-4 summary p-4">
+                        <CustomerAdd onAdd={onCustomerAdd}/>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
+export default App;
