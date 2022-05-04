b307e01bbafa0eb80ba9bd2a6956ace64432d597
jinhyung <pjh_jn@naver.com>
2022-05-04 15:53:03 +0900

    feat : 리액트 Designer 컴포넌트 추가

---
+++ kdt-react-order-ui/src/components/Designer.js
@@ -0,0 +1,34 @@
+import React from "react";
+import axios from "axios";
+
+export function Designer(props) {
+  const id = props.id;
+  const name = props.name;
+  const position = props.position;
+  const  joinedAt = props.joinedAt;
+
+  const handleSubmit = (e) => {
+    axios({
+      url:`http://localhost:8080/api/v1/designers/`+id,
+      method : 'delete'
+    }
+
+    ).then(
+        v => {alert("디자이너 정보가 삭제되었습니다.")
+        props.onRemove(id);
+        },
+        e => {
+          alert("서버 장애");
+          console.error(e);
+        })
+  }
+  return <>
+
+    <div className="col">
+      <div className="row text-muted">{id}</div>
+      <div className="row">{name}</div>
+    </div>
+    <div className="col text-center position">{position}</div>
+    <button className="btn btn-small btn-outline-dark" onClick={handleSubmit}>삭제</button>
+  </>
+}
