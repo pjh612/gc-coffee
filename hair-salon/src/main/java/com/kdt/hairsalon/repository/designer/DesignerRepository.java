7b2746097945af765f27bef02baa280c3edb35d7
jinhyung <pjh_jn@naver.com>
2022-05-05 13:46:54 +0900

    refactor : 스타일링

--- hair-salon/src/main/java/com/kdt/hairsalon/repository/designer/DesignerRepository.java
+++ hair-salon/src/main/java/com/kdt/hairsalon/repository/designer/DesignerRepository.java
@@ -1,7 +1,6 @@
 package com.kdt.hairsalon.repository.designer;
 
 import com.kdt.hairsalon.model.Designer;
-
 import com.kdt.hairsalon.model.Position;
 
 import java.util.List;
@@ -10,9 +9,12 @@ import java.util.UUID;
 
 public interface DesignerRepository {
     Designer insert(Designer designer);
+
     Optional<Designer> findById(UUID id);
 
     List<Designer> findAll();
+
     void deleteById(UUID id);
+
     UUID update(UUID id, String name, Position position);
 }
