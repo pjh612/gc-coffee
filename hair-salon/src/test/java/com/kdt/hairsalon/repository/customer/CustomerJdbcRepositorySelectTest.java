5e8f243aafa0a952b3492b60032418535df2c49b
jinhyung <pjh_jn@naver.com>
2022-05-05 13:25:19 +0900

    test : CustomerJdbcRepositoryTest 조회, 업데이트 테스트 분리

---
+++ hair-salon/src/test/java/com/kdt/hairsalon/repository/customer/CustomerJdbcRepositorySelectTest.java
@@ -0,0 +1,134 @@
+package com.kdt.hairsalon.repository.customer;
+
+import com.kdt.hairsalon.model.Customer;
+import com.kdt.hairsalon.model.Gender;
+import com.wix.mysql.EmbeddedMysql;
+import com.wix.mysql.config.Charset;
+import com.wix.mysql.config.MysqldConfig;
+import com.wix.mysql.distribution.Version;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static com.wix.mysql.EmbeddedMysql.anEmbeddedMysql;
+import static com.wix.mysql.ScriptResolver.classPathScript;
+import static com.wix.mysql.config.MysqldConfig.aMysqldConfig;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ActiveProfiles("test")
+public class CustomerJdbcRepositorySelectTest {
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    private static EmbeddedMysql embeddedMySql;
+
+    Customer customerA = new Customer(
+            UUID.randomUUID(),
+            "customerA",
+            "customerA@gmail.com",
+            Gender.MAN, LocalDate.now(),
+            LocalDateTime.now(),
+            LocalDateTime.now()
+    );
+
+    Customer customerB = new Customer(
+            UUID.randomUUID(),
+            "customerB",
+            "customerB@gmail.com",
+            Gender.MAN, LocalDate.now(),
+            LocalDateTime.now(),
+            LocalDateTime.now()
+    );
+
+    Customer customerC = new Customer(
+            UUID.randomUUID(),
+            "customerC",
+            "customerC@gmail.com",
+            Gender.WOMAN, LocalDate.now(),
+            LocalDateTime.now(),
+            LocalDateTime.now()
+    );
+
+    @BeforeAll
+    void setup() {
+        MysqldConfig config = aMysqldConfig(Version.v5_7_latest)
+                .withCharset(Charset.UTF8)
+                .withPort(2215)
+                .withUser("test", "test1234!")
+                .withTimeZone("Asia/Seoul")
+                .build();
+        embeddedMySql = anEmbeddedMysql(config)
+                .addSchema("test-hair_salon", classPathScript("schema.sql"))
+                .start();
+        customerRepository.insert(customerA);
+        customerRepository.insert(customerB);
+        customerRepository.insert(customerC);
+    }
+
+    @AfterAll
+    static void cleanUp() {
+        embeddedMySql.stop();
+    }
+
+    @Test
+    @DisplayName("ID로 Customer 조회")
+    void findById() {
+        //when
+        Optional<Customer> insertedCustomer = customerRepository.findById(customerA.getId());
+
+        //then
+        assertThat(insertedCustomer.isPresent(), is(true));
+        assertThat(insertedCustomer.get(), samePropertyValuesAs(customerA));
+    }
+
+    @Test
+    void findByName() {
+        Customer sameNameCustomer = new Customer(
+                UUID.randomUUID(),
+                "customerA",
+                "customerD@gmail.com",
+                Gender.MAN, LocalDate.now(),
+                LocalDateTime.now(),
+                LocalDateTime.now()
+        );
+        customerRepository.insert(sameNameCustomer);
+
+        //when
+        List<Customer> customers = customerRepository.findByName(sameNameCustomer.getName());
+
+        //then
+        assertThat(customers.size(), is(2));
+        assertThat(customers, containsInAnyOrder(samePropertyValuesAs(customerA), samePropertyValuesAs(sameNameCustomer)));
+    }
+
+    @Test
+    @DisplayName("이메일로 Customer 조회")
+    void findByEmail() {
+        //when
+        Optional<Customer> foundCustomer = customerRepository.findByEmail(customerA.getEmail());
+
+        //then
+        assertThat(foundCustomer.isPresent(), is(true));
+        assertThat(foundCustomer.get(), samePropertyValuesAs(customerA));
+    }
+
+    @Test
+    @DisplayName("모든 Customer 조회 테스트")
+    void findAll() {
+        List<Customer> customers = customerRepository.findAll();
+
+        //then
+        assertThat(customers.size(), is(3));
+        assertThat(customers, containsInAnyOrder(samePropertyValuesAs(customerA), samePropertyValuesAs(customerB), samePropertyValuesAs(customerC)));
+    }
+}
--- hair-salon/src/test/java/com/kdt/hairsalon/repository/customer/CustomerJdbcRepositoryTest.java
+++ hair-salon/src/test/java/com/kdt/hairsalon/repository/customer/CustomerJdbcRepositoryTest.java
@@ -10,12 +10,10 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataAccessException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -23,11 +21,11 @@ import static com.wix.mysql.EmbeddedMysql.anEmbeddedMysql;
 import static com.wix.mysql.ScriptResolver.classPathScript;
 import static com.wix.mysql.config.MysqldConfig.aMysqldConfig;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.samePropertyValuesAs;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ActiveProfiles("test")
 class CustomerJdbcRepositoryTest {
@@ -36,24 +34,6 @@ class CustomerJdbcRepositoryTest {
     private CustomerRepository customerRepository;
 
     private static EmbeddedMysql embeddedMySql;
-
-    private final Customer customerA = new Customer(
-            UUID.randomUUID(),
-            "park",
-            "customerA@gmail.com",
-            Gender.MAN, LocalDate.now(),
-            LocalDateTime.now(),
-            LocalDateTime.now()
-    );
-
-    private final Customer customerB = new Customer(
-            UUID.randomUUID(),
-            "park",
-            "customerB@gmail.com",
-            Gender.WOMAN, LocalDate.now(),
-            LocalDateTime.now(),
-            LocalDateTime.now()
-    );
 
     @BeforeAll
     void setup() {
@@ -66,7 +46,6 @@ class CustomerJdbcRepositoryTest {
         embeddedMySql = anEmbeddedMysql(config)
                 .addSchema("test-hair_salon", classPathScript("schema.sql"))
                 .start();
-
     }
 
     @AfterAll
@@ -75,92 +54,72 @@ class CustomerJdbcRepositoryTest {
     }
 
     @Test
-    @Order(1)
     @DisplayName("Customer 정보 정상 추가")
     void insertTest() {
+        //given
+        Customer customer = new Customer(
+                UUID.randomUUID(),
+                "customerA",
+                "customerA@gmail.com",
+                Gender.MAN, LocalDate.now(),
+                LocalDateTime.now(),
+                LocalDateTime.now());
+
         //when
-        customerRepository.insert(customerA);
-        Optional<Customer> insertedCustomer = customerRepository.findById(customerA.getId());
+        customerRepository.insert(customer);
+        Optional<Customer> insertedCustomer = customerRepository.findById(customer.getId());
 
         //then
         assertThat(insertedCustomer.isPresent(), is(true));
-        assertThat(insertedCustomer.get(), samePropertyValuesAs(customerA));
+        assertThat(insertedCustomer.get(), samePropertyValuesAs(customer));
     }
 
     @Test
-    @Order(2)
     @DisplayName("Customer끼리 이메일이 중복될 수 없다.")
     void insertWithDuplicatedEmailTest() {
         //given
-        Customer duplicatedEmailCustomer = new Customer(
+        Customer customer = new Customer(
                 UUID.randomUUID(),
-                "lee",
-                "customerA@gmail.com",
+                "customerB",
+                "customerB@gmail.com",
                 Gender.MAN, LocalDate.now(),
                 LocalDateTime.now(),
                 LocalDateTime.now()
         );
+
+        Customer duplicatedEmailCustomer = new Customer(
+                UUID.randomUUID(),
+                "customerB",
+                "customerB@gmail.com",
+                Gender.MAN, LocalDate.now(),
+                LocalDateTime.now(),
+                LocalDateTime.now()
+        );
+        customerRepository.insert(customer);
+
         //when, then
         assertThrows(DataAccessException.class, () -> customerRepository.insert(duplicatedEmailCustomer));
-
     }
 
     @Test
-    @Order(3)
-    @DisplayName("ID로 Customer 조회")
-    void findById() {
-        //when
-        Optional<Customer> insertedCustomer = customerRepository.findById(customerA.getId());
-
-        //then
-        assertThat(insertedCustomer.isPresent(), is(true));
-        assertThat(insertedCustomer.get(), samePropertyValuesAs(customerA));
-    }
-
-    @Test
-    @Order(4)
-    @DisplayName("동명이인 조회")
-    void findByName() {
-        //given
-        customerRepository.insert(customerB);
-
-        //when
-        List<Customer> customers = customerRepository.findByName(customerA.getName());
-
-        //then
-        assertThat(customers.size(), is(2));
-        assertThat(customers, containsInAnyOrder(samePropertyValuesAs(customerA), samePropertyValuesAs(customerB)));
-    }
-
-    @Test
-    @Order(5)
-    @DisplayName("이메일로 Customer 조회")
-    void findByEmail() {
-        Optional<Customer> foundCustomer = customerRepository.findByEmail(customerB.getEmail());
-
-        assertThat(foundCustomer.isPresent(), is(true));
-        assertThat(foundCustomer.get(), samePropertyValuesAs(customerB));
-    }
-
-    @Test
-    @Order(6)
-    @DisplayName("모든 Customer 조회 테스트")
-    void findAll() {
-        List<Customer> customers = customerRepository.findAll();
-
-        //then
-        assertThat(customers.size(), is(2));
-        assertThat(customers, containsInAnyOrder(samePropertyValuesAs(customerA), samePropertyValuesAs(customerB)));
-    }
-
-    @Test
-    @Order(7)
     @DisplayName("Customer 삭제 테스트")
     void deleteByIdTest() {
+        //given
+        Customer customer = new Customer(
+                UUID.randomUUID(),
+                "customerG",
+                "customerG@gmail.com",
+                Gender.MAN, LocalDate.now(),
+                LocalDateTime.now(),
+                LocalDateTime.now()
+        );
+        customerRepository.insert(customer);
+
         //when
-        customerRepository.deleteById(customerA.getId());
+        customerRepository.deleteById(customer.getId());
+        Optional<Customer> foundCustomer = customerRepository.findById(customer.getId());
 
         //then
-        assertThrows(EmptyResultDataAccessException.class, () -> customerRepository.findById(customerA.getId()));
+        assertThat(foundCustomer.isEmpty(), is(true));
     }
 }
