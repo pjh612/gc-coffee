d5abd04add5aa03af0175c847b74b58c5f78561b
jinhyung <pjh_jn@naver.com>
2022-05-06 02:10:53 +0900

    fix : 미사용 request 필드 제거

--- hair-salon/src/main/java/com/kdt/hairsalon/controller/api/request/UpdateAppointmentRequest.java
+++ hair-salon/src/main/java/com/kdt/hairsalon/controller/api/request/UpdateAppointmentRequest.java
@@ -6,6 +6,6 @@ import javax.validation.constraints.Future;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record UpdateAppointmentRequest(UUID appointmentId, UUID menuId, AppointmentStatus status,
+public record UpdateAppointmentRequest(UUID appointmentId, AppointmentStatus status,
                                        @Future(message = "잘못된 예약 시간입니다.") LocalDateTime appointedAt) {
 }
